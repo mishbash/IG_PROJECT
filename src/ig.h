433e511ca9303ab6791e7e42fe29e5d818a0d44b
mishbash <138617704+mishbash@users.noreply.github.com>
2023-08-25 15:25:47 +0300

    Update ig.h

--- src/ig.h
+++ src/ig.h
@@ -8,12 +8,14 @@
 struct IGNode {
     std::vector<unsigned> children;
     std::vector<bool> negate;
-    Function func;
+    Function function;
 
     void connect(unsigned child, bool neg);
 
     IGNode();
     IGNode(std::vector<unsigned> children, std::vector<bool> negate, std::vector<bool> values);
+    IGNode(std::vector<unsigned> children, std::vector<bool> negate, Function function);
+
 };
 
 class IG {
@@ -30,6 +32,10 @@ public:
     bool getNegateOutput() const;
     void setNegateOutput(bool value);
 
+    std::vector<Function>& getFunctions();
+    const std::vector<Function>& getFunctions() const;
+    bool addFunction(Function new_function);
+
     std::vector<IGNode>& getNodes();
     const std::vector<IGNode>& getNodes() const;
     unsigned addNode(const IGNode &node);
@@ -37,17 +43,18 @@ public:
     bool hasCycles() const;
 
     unsigned getComplexity() const;
-    Function getFunction() const;
+    Function getResult() const;
 
     void read(std::istream &istream);
     void print(std::ostream& ostream) const;
+    void MKSprint(std::ostream& ostream) const;
 
 private:
     unsigned variable_count;
     unsigned output;
     bool negate_output;
+    std::vector<Function> functions;
     std::vector<IGNode> nodes;
 
     bool traverse(const std::vector<bool>& x, unsigned v, std::vector<bool>& used, std::vector<bool>& used_res) const;
 };
-
