83bda03ac2b3f6d680612dac19c8cdf50609bb71
mishbash <138617704+mishbash@users.noreply.github.com>
2023-08-25 15:27:52 +0300

    Update ig.cpp

--- src/ig.cpp
+++ src/ig.cpp
@@ -2,24 +2,30 @@
 
 #include <ranges>
 #include <string>
+#include <cassert>
+#include <iomanip>
 
-IGNode::IGNode() : children(0), negate(0), func(0) {}
+IGNode::IGNode() : children(0), negate(0), function(0) {}
 
 IGNode::IGNode(std::vector<unsigned> children, std::vector<bool> negate, std::vector<bool> values) : 
-    children(children), negate(negate), func(values) {}
+    children(children), negate(negate), function(values)  {}
+
+IGNode::IGNode(std::vector<unsigned> children, std::vector<bool> negate, Function function) : 
+    children(children), negate(negate), function(function)  {}
 
 void IGNode::connect(unsigned child, bool neg) {
     children.push_back(child);
     negate.push_back(neg);
 }
 
-IG::IG() : variable_count(5), output(0), negate_output(false), nodes() {}
+IG::IG() : variable_count(4), output(0), negate_output(false), nodes() {}
 
 IG::~IG() {}
 
 
 unsigned IG::addNode(const IGNode &node) {
     nodes.push_back(node);
+    addFunction(node.function);
     return nodes.size() + variable_count;
 }
 
@@ -48,11 +54,11 @@ bool IG::traverse(const std::vector<bool>& x, unsigned v, std::vector<bool>& use
         ++i;
     }
 
-    return used_res[v] = node.func.getValue(t_table);
+    return used_res[v] = node.function.getValue(t_table);
 }
 
 
-Function IG::getFunction() const {
+Function IG::getResult() const {
     std::vector<bool> x(variable_count, false);
     Function f(variable_count);
     for (unsigned i = 0; i < (1 << variable_count); ++i) {
@@ -95,9 +101,8 @@ void IG::read(std::istream &istream) {
         for (auto [index, value] : std::views::enumerate(new_node_function)) {
             value = s[index] == '1';
         }
-        
 
-        node.func = Function(new_node_function);
+        node.function = Function(new_node_function);
 
         nodes.push_back(node);
     }
@@ -119,10 +124,27 @@ void IG::print(std::ostream &ostream) const {
             ostream << n << " ";
         }
         ostream << "\nFunction\n";
-        ig_node.func.print(ostream);
+        ig_node.function.print(ostream);
         ostream << "\n";
         ++count;
     }
+}
+
+void IG::MKSprint(std::ostream& ostream) const {
+    auto result = getResult();
+
+    ostream << std::setw(getVariableCount()) << result.mincode() << " ";
+    ostream << getComplexity() << " ";
+    ostream << output << " " << negate_output << " ";
+
+    for (auto& ig_node : nodes) {
+        assert(ig_node.children.size() == 2);
+        assert(ig_node.negate.size() == 2);
+        ostream << ig_node.children[0] << " " << ig_node.negate[0] << " ";
+        ostream << ig_node.children[1] << " " << ig_node.negate[1] << " ";
+
+    }
+    ostream << std::endl;
 }
 
 unsigned IG::getVariableCount() const {
@@ -161,8 +183,23 @@ unsigned IG::getComplexity() const {
     return nodes.size();
 }
 
+std::vector<Function>& IG::getFunctions() {
+    return functions;
+}
 
+const std::vector<Function>& IG::getFunctions() const {
+    return functions;
+}
 
+bool IG::addFunction(Function new_function) {
+    for (auto i : functions) {
+        if (new_function.mincode() == i.mincode()) {
+            return false;
+        }
+    }
+    functions.push_back(new_function);
+    return true;
+}
 
 
 
