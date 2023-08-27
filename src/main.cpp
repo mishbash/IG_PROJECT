19be859cb12ec2e3f399a1f2e0ffe6115e306c5f
mishbash <138617704+mishbash@users.noreply.github.com>
2023-08-27 18:59:40 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,56 @@
 #include "ig.h"
+#include "ig_bruteforce.h"
 
 #include <iostream>
 #include <fstream>
 
+std::ofstream file;
+
 int main() {
-    IG test;
+    // IG test;
 
-    // test for all variables AND
-    // test.setVariableCount(4);
-    // std::vector<unsigned> con{3, 4};
-    // std::vector<bool> negate{false, false};
-    // std::vector<bool> table{0,1,1,0};
-    // IGNode test_node(con, negate, table);
-    // test.addNode(test_node);
+    // test.setVariableCount(5);
 
-    // std::vector<unsigned> con1{1, 2};
-    // std::vector<bool> negate1{false, false};
-    // std::vector<bool> table1{0,1,1,0};
-    // IGNode test_node1(con1, negate1, table1);
-    // test.addNode(test_node1);
-
-    // std::vector<unsigned> con2{5, 6};
-    // std::vector<bool> negate2{false, false};
-    // std::vector<bool> table2{0,1,1,0};
-    // IGNode test_node2(con2, negate2, table2);
-    // test.addNode(test_node2);
-
-
-    // test for XOR on 2 variables
-    // test.setVariableCount(2);
-    // std::vector<unsigned> con{1, 2};
-    // std::vector<bool> negate{true, false};
-    // std::vector<bool> table{0,0,0,1};
-    // IGNode test_node(con, negate, table);
-    // test.addNode(test_node);
-
-    // std::vector<unsigned> con1{1, 2};
-    // std::vector<bool> negate1{false, true};
-    // std::vector<bool> table1{0,0,0,1};
-    // IGNode test_node1(con1, negate1, table1);
-    // test.addNode(test_node1);
-
-    // std::vector<unsigned> con2{3, 4};
-    // std::vector<bool> negate2{true, true};
-    // std::vector<bool> table2{0,0,0,1};
-    // IGNode test_node2(con2, negate2, table2);
-    // test.addNode(test_node2);
-
-    // test.setOutput(5);
-    // test.setNegateOutput(true);
+    // std::ifstream inputFile("./src/input.txt");
+    // if (inputFile.is_open()) {
+    //     test.read(inputFile);
+    //     inputFile.close();
+    // } else {
+    //     std::cout << "ERROR OPENING FILE!\n";
+    //     return 0;
+    // }
 
     // test.print(std::cout);
 
-    // auto f = test.getFunction();
-    // f.bigPrint(std::cout);
+    // auto f = test.getResult();
+    // f.print(std::cout);
+    // // f.bigPrint(std::cout);
 
-    test.setVariableCount(2);
+    // IG test;
+    // test.setVariableCount(2);
+    // test.addFunction(Function({0,0,0,1}));
+    // IGBruteforcer bruteforce_test;
+    // bruteforce_test.bruteforce(test, 2);
 
-    std::ifstream inputFile("./src/input.txt");
-    if (inputFile.is_open()) {
-        test.read(inputFile);
-        inputFile.close();
-    } else {
-        std::cout << "ERROR OPENING FILE!\n";
-        return 0;
-    }
+    file.open("output.txt", std::ios::out);
 
-    // test.read(std::cin);
+    IG aig_test;
+    aig_test.setVariableCount(4);
+    aig_test.addFunction(Function({0,0,0,1}));
+    IGBruteforcer bruteforce_aig_test;
+    bruteforce_aig_test.bruteforce(aig_test, 5);
 
-    test.print(std::cout);
 
-    auto f = test.getFunction();
-    f.bigPrint(std::cout);
+    // IG xaig_test;
+    // xaig_test.setVariableCount(4);
+    // xaig_test.addFunction(Function({0,0,0,1}));
+    // xaig_test.addFunction(Function({0,1,1,0}));
+    // IGBruteforcer bruteforce_xaig_test;
+    // bruteforce_xaig_test.bruteforce(xaig_test, 5);
+
 
     return 0;
 }
+
+// mincode, complex, graph (MKS), 
+// not found for this
